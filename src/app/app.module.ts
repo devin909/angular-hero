b100613691d67bac7450002d79497c49c643605b
Devin Lee <devin.lee@coveros.com>
2017-12-12 15:23:02 -0500

    updated vulnerable package, and added delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroService } from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+  AppComponent,
+  HeroesComponent,
+  HeroDetailComponent,
+  MessagesComponent,
+  DashboardComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
+  BrowserModule,
+  FormsModule,
+  AppRoutingModule,
+  HttpClientModule,
+  HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
